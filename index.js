5f7230cc32dee3bb700068bce51ca95caf12b58e
Reiner Mangly <manglynho@gmail.com>
2024-11-06 17:17:29 +0000

    forgot body parser

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 require('dotenv').config()
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
